108934526d2d12830cf5954202a759bc2ccc3cbc
Andre Vu <andre.vu.ms@gmail.com>
2024-09-13 12:02:02 -0700

    added andreSeed.js file

---
+++ prisma/andreSeed.js
@@ -0,0 +1,34 @@
+const CreateItem = async () => {
+    const item = [
+      {
+        id: 1,
+        seller_id: 1,
+        name: "Laura Piglet",
+        price: $5,
+        description: "Perfect 2 piece dress for Tulum"
+        default_photo
+        additional_photos []
+        tags: [tulum, summer, dress]
+      },
+
+          {
+        id: 2,
+        seller_id: 2,
+        name: "Melissa Cat",
+        price: $10,
+        description: "Cute hawaii shirt for your travels"
+        default_photo
+        additional_photos []
+        tags: [shirt, hawaii, travel]
+      },
+
+              {
+        id: 3,
+        seller_id: 3,
+        name: "Roger Rabbit",
+        price: $20,
+        description: "Dress shirt for going out"
+        default_photo
+        additional_photos []
+        tags: [dressy, red, nightout]
+      },
