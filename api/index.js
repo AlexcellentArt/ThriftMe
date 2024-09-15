726255a810b4f38667b2944088fa574c8e7a07b4
Alexis <alexcellentart@gmail.com>
2024-09-15 19:17:38 -0400

    Fixed Server and Index. They will now only make and require the base paths found. I've structured it so that if we need to do any refactoring later, all we need to do is modify the api_list array in api/index and that'll handle it. I've also made it so that after this is all loaded, in the console log there will be a bright and color coded table of the API Status, showing what's found and functioning, what's missing, and what's broken followed in the same order by the errors that broke it.

--- api/index.js
+++ api/index.js
@@ -1,22 +1,8 @@
 const router = require("express").Router();
 module.exports = router;
-const e = require("express");
 const fs = require('fs');
-
 // massive check
-const api_list = ["user","item","checkout","shopping_cart","past_transactions","browsing_history","skibidi"]
-async function checkFileExists(file) {
-    // return fs.access(`${file}`, fs.constants.F_OK, (err) => {if (err){console.log("ERRR");return false} return true})
-  try {
-    fs.access(file, fs.promises.constants.F_OK);
-    // const res = fs.access(`${file}`, fs.constants.F_OK, (err) => {if (err){return false} return true});
-    // console.log(res)
-    // if (res){ return false}
-    return true;
-  } catch {
-    return false
-  }
-}
+const api_list = ["user","item","checkout","shopping_cart","past_transactions","browsing_history"]
 function doFilesExist(fileList) {
     const table = {"yes":[],"no":[]}
     for (let index = 0; index < fileList.length; index++) {
@@ -27,74 +13,48 @@ function doFilesExist(fileList) {
 function mapToPath(arr,path="api",fileType="js"){
     return arr.map((file)=>`${path}/${file}.${fileType}`)
 }
+function mapToBaseNames(path){return path.split("/").pop().split(".").shift()}
 async function requireExistingAPI(arr) {
-    console.log("✓ = found || Not Found")
-    const table = doFilesExist(mapToPath(api_list))
-    // list.yes.forEach(router.use("/user", require("./user"));)
-    
-    // table.yes.forEach(router.use("/user", require("./user"));)
-// api_list.forEach((api)=>{
-//     if (checkFileExists(api) === undefined){table.yes.push(api)}
-//     else {table.no.push(api)}
-    
-// })
-// for await (const element of object) {
-    
-// }
-// for (let i = 0; i < api_list.length; i++) {
-//     const element = api_list[i];
-//     const exists = await checkFileExists(element)
-//     if (exists === undefined){table.yes.push(element)}
-//     else {table.no.push(element)
-    
-// }
-console.log("Checks completed.\u001b[0m")
-console.log("\u001b[1;93m==================")
-console.log("\u001b[1;92m✓ Found ✓ \u001b[0m")
-table.yes.forEach((api)=>console.log(`\u001b[1;32m ✓ ${api} \u001b[0m`))
-console.log("\u001b[1;93m------------------")
-console.log("\u001b[1;91m✖ Missing ✖ \u001b[0m")
-console.log("\u001b[1;93m------------------")
-table.no.forEach((api)=>console.error(`✖ ${api}\u001b[0m`))
-console.log("\u001b[1;93m==================")
-console.log("Checks completed.\u001b[0m")
+    const table = {"found":[],"broken":[],"missing":[]}
+    const errors = []
+    const doExist = doFilesExist(mapToPath(api_list))
+    table.missing = doExist.no.map(mapToBaseNames)
+    doExist.yes.map(mapToBaseNames).forEach((found)=>{try {
+        router.use(`/${found}`, require(`./${found}`))
+        table.found.push(found)
+    } catch (error)
+    {
+        errors.push(error)
+        table.broken.push(found)
+    }})
+console.log("\u001b[1;96m==================")
+console.log("\u001b[1;95mAPI Status\u001b[0m")
+if (table.found.length){
+    console.log("\u001b[1;97m------------------\u001b")
+    console.log("\u001b[1;92m✓ Found ✓ \u001b[0m")
+    table.found.forEach((api)=>console.log(`\u001b[1;32m ✓ ${api} \u001b[0m`))
 }
+if (table.missing.length){
+    console.log("\u001b[1;97m------------------\u001b")
+    console.log("\u001b[1;91m✖ Missing ✖ \u001b[0m")
+    table.missing.forEach((api)=>console.error(`✖ ${api}\u001b[0m`))
+}
+if (table.broken.length)
+{
+    console.log("\u001b[1;97m------------------\u001b[0m")
+    console.log("\u001b[1;93m? Broken ?\u001b[0m")
+    table.broken.forEach((api)=>console.log(`\u001b[1;93m? ${api}\u001b[0m`))
+}
+console.log("\u001b[1;97m------------------\u001b[0m")
+if (errors.length)
+{
+    console.log("\u001b[1;95m ? ERRORS ? \u001b[0m")
+    errors.map(console.error)
+}
+console.log("\u001b[1;96m==================\u001b[0m")
+}
+// function makeTable(table,columnStyling)
+// {
+//     // will go back at some point in the future to make this generic
+// }
 requireExistingAPI(api_list)
-// console.log("✓ = found || Not Found")
-// api_list.forEach((api)=>{checkFileExists(api)})
-// console.log("Checks completed.\u001b[0m")
-// /api
-if (fs.existsSync("./user"))
-{
-    router.use("/user", require("./user"));
-}
-else {
-    console.log("user does not exist")
-}
-if (fs.existsSync("./item"))
-{
-    router.use("/item", require("./item"));
-}
-
-if (fs.existsSync("./shopping_cart"))
-{
-    router.use("/shopping_cart", require("./shopping_cart"));
-}
-
-if (fs.existsSync("./past_transactions"))
-{
-    router.use("/past_transactions", require("./past_transactions"));
-}
-
-if (fs.existsSync("./checkout"))
-{
-    router.use("/checkout", require("./checkout"));
-}
-if (fs.existsSync("./credit_card"))
-{
-    router.use("/credit_card", require("./credit_card"));
-}
-if (fs.existsSync("./addresses"))
-{
-    router.use("/addresses", require("./addresses"));
-}
