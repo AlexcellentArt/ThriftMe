d5a904d75428ab9e936852525e2fd08d00210ba8
Alexis <alexcellentart@gmail.com>
2024-09-17 22:20:14 -0400

    Basic css done to make header and footer distinguishable from main

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -1,11 +1,6 @@
-import { AuthContext } from "./AuthContext";
-import { useContext } from "react";
-function PageWrapper() {
-  // setState searchTags, should be an array of strings
-  const { token } = useContext(AuthContext);
-  // You are gonna want an input type search inside the div. Next to it you are gonna want a button with # inside it.
-  // setState stringArray
-  // relevant docs: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/search
+import thriftmeLogo from "/src/assets/ThriftMeLogo.svg";
+import Navigations from "./Navigations";
+function PageWrapper({children}) {
   return (
     <>
       <header>
@@ -14,7 +9,7 @@ function PageWrapper() {
         </div>
         <Navigations/>
       </header>
-      <main></main>
+      <main>{children}</main>
       <footer>This is A Footer in PageWrapper. If you see this</footer>
     </>
   );
