1afd9d2e0a02e11aad96106ffa2a011a1a36b511
Andre Vu <andre.vu.ms@gmail.com>
2024-09-19 11:55:15 -0700

    added additionalData.js file for item and user dummy data

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -4,6 +4,9 @@ import { useParams } from "react-router-dom";
 function SingleProduct() {
     const {token} = useContext(AuthContext);
     // in useEffect, make a fetch get call to items/:id
+    useEffect(() => {
+        
+    }, []);
     // Could probably get away with using a DisplayMany for the tags and another one for the additional photos
     // to see an example of how to use DisplayMany, look at SearchBar, where it is used to handle tags
     /// consult the Search Views / Products Page on our mockframe for how it should look
