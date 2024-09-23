399cdb77a911383100405ad89ebc63da13202b6b
Andre Vu <andre.vu.ms@gmail.com>
2024-09-23 09:26:17 -0700

    initial commit for addresses.js and credit_cards.js

--- api/addresses.js
+++ api/addresses.js
@@ -5,30 +5,31 @@ const prisma = require("../prisma");
 const gen_errors = require("./helpers/gen_errors.js")
 // THINGS TO REPLACE TO GET FUNCTIONAL:
 
-// REPLACE_THIS_WITH_MODEL_NAME using ctrl+f or other hot key to find all and replace this with the model being interacted with. EXAMPLE: past_Transactions.
-// INSERT_DATA_HERE: replace with the data being sent to table in a post or update func.  EXAMPLE: {name:"Kelly","email":"kelly@gmail", "password":"kellyRulez"}
-// REPLACE_WITH_WHAT_DATA_YOU_WANT: replace with the data you want deconstructed out of req.body  EXAMPLE: {name,email,password}
-// THE PATHS: I left them blank so you could structure them as needed.
+//#1 REPLACE_THIS_WITH_MODEL_NAME ctrl+f or other hot key to find all and replace this with the model being interacted with. EXAMPLE: past_Transactions.
+//#2 INSERT_DATA_HERE: replace with the data being sent to table in a post or update func.  EXAMPLE: {name:"Kelly","email":"kelly@gmail", "password":"kellyRulez"}
+//#3 REPLACE_WITH_WHAT_DATA_YOU_WANT: replace with the data you want deconstructed out of req.body  EXAMPLE: {name,email,password}
+//#4 THE PATHS: I left them blank so you could structure them as needed.
+
 // ### GET ###
 
-// Gets all REPLACE_THIS_WITH_MODEL_NAME
+// Gets all addresses
 router.get("/", async (req, res, next) => {
     try {
-      const REPLACE_THIS_WITH_MODEL_NAME = await prisma.REPLACE_THIS_WITH_MODEL_NAME.findMany();
-      res.json(REPLACE_THIS_WITH_MODEL_NAME);
+      const addresses = await prisma.addresses.findMany();
+      res.json(addresses);
     } catch(error) {
         next(error);
         }
   });
-  // Returns REPLACE_THIS_WITH_MODEL_NAME matching id
+  // Returns addresses matching id
   router.get("/:id", async (req, res, next) => {
     try {
       const id = +req.params.id;
-      const REPLACE_THIS_WITH_MODEL_NAME = await prisma.REPLACE_THIS_WITH_MODEL_NAME.findUnique({ where: { id } });
-      if (!REPLACE_THIS_WITH_MODEL_NAME) {
-        return next(gen_errors.genericNotFoundError("REPLACE_THIS_WITH_MODEL_NAME","id",id));
+      const addresses = await prisma.addresses.findUnique({ where: { id } });
+      if (!addresses) {
+        return next(gen_errors.genericNotFoundError("addresses","id",id));
       }
-      res.json(REPLACE_THIS_WITH_MODEL_NAME);
+      res.json(addresses);
     } catch(error) {
         next(error);
         }
@@ -40,8 +41,8 @@ router.post("/", async (req, res, next) => {
       const { REPLACE_WITH_WHAT_DATA_YOU_WANT } = await req.body;
       // write your own checks to validate obj here and if it fails, run next(genericMissingDataError(missingValues,forWhat))
       // ex: if {!name} {next(genericMissingDataError("name","user"))}
-      const REPLACE_THIS_WITH_MODEL_NAME = await prisma.REPLACE_THIS_WITH_MODEL_NAME.create({ data: {INSERT_DATA_HERE}});
-      res.json(REPLACE_THIS_WITH_MODEL_NAME);
+      const addresses = await prisma.addresses.create({ data: {INSERT_DATA_HERE}});
+      res.json(addresses);
     } catch (error) {
       next(error)
     }
@@ -49,38 +50,38 @@ router.post("/", async (req, res, next) => {
 // ### PUT ###
 
 
-  // Updates REPLACE_THIS_WITH_MODEL_NAME
+  // Updates addresses
   router.put("/:id", async (req, res, next) => {
     try {
       const id = +req.params.id;
-      const exists = await prisma.REPLACE_THIS_WITH_MODEL_NAME.findUnique({ where: { id } });
+      const exists = await prisma.addresses.findUnique({ where: { id } });
       if (!exists) {
-        return next(gen_errors.genericNotFoundError("REPLACE_THIS_WITH_MODEL_NAME","id",id));
+        return next(gen_errors.genericNotFoundError("addresses","id",id));
       }
 		const { REPLACE_WITH_WHAT_DATA_YOU_WANT } = await req.body;
       // write your own checks to validate obj here and if it fails, run next(genericMissingDataError(missingValues,forWhat))
       // ex: if {!name} {next(genericMissingDataError("name","user"))}
-      const REPLACE_THIS_WITH_MODEL_NAME = await prisma.REPLACE_THIS_WITH_MODEL_NAME.update({
+      const addresses = await prisma.addresses.update({
         where: { id },
         data: { INSERT_DATA_HERE },
       });
-      res.json(REPLACE_THIS_WITH_MODEL_NAME);
+      res.json(addresses);
     } catch(error) {
     next(error);
     }
   });
 
 // ### DELETE ###
-  // deletes REPLACE_THIS_WITH_MODEL_NAME matching id
+  // deletes addresses matching id
 router.delete("/:id", async (req, res, next) => {
 
     try {
       const id = +req.params.id;
-      const exists = await prisma.REPLACE_THIS_WITH_MODEL_NAME.findUnique({ where: { id } });
+      const exists = await prisma.addresses.findUnique({ where: { id } });
       if (!exists) {
-        return next(gen_errors.genericNotFoundError("REPLACE_THIS_WITH_MODEL_NAME","id",id));
+        return next(gen_errors.genericNotFoundError("addresses","id",id));
       }
-      await prisma.REPLACE_THIS_WITH_MODEL_NAME.delete({ where: { id } });
+      await prisma.addresses.delete({ where: { id } });
       res.sendStatus(204);
     } catch(error) {
     next(error);
