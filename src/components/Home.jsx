12ce2f58cc045ad924dd605ce9086f4edda34b74
Sujoy Manir <sujoymanir@gmail.com>
2024-09-23 10:41:11 -0400

    Changes to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,16 @@
 import DisplayMany from "./DisplayMany";
 import { useState,useEffect } from "react";
 import { useParams,useNavigate } from "react-router-dom";
+import { AuthContext } from "./AuthContext";
+import { useEffect } from "react";
+
+
+
 function Home() {
+
+const [normal, updated_normal_page]= useState();
+
+const response=
     return (<div>HOME PAGE</div>);
 }
 
