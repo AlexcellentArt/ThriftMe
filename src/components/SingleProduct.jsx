d821cb60d4339e511ec92b5fdba55ff3aac43aae
Alexis <alexcellentart@gmail.com>
2024-09-23 12:11:51 -0400

    tags and additional photos fixed

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -4,10 +4,27 @@ import { useParams } from "react-router-dom";
 import DisplayMany from "./DisplayMany";
 
 function SingleProduct() {
-   const {token} = useContext(AuthContext);
+   const {token,addToCart,toggleFavorite} = useContext(AuthContext);
    const {id} = useParams();
 
    const [product, setProduct] = useState(null);
+   function createTag(obj) {
+    console.log(obj)
+    return (
+      <div className="tag">
+        <p>#{obj.text}</p>
+      </div>
+    );
+  }
+function createPhoto(obj) {
+    console.log(obj)
+    return (
+      <div className="photo">
+        <img className ="square photo fit-to-parent"src={obj.photo} alt={obj.photo} />
+      </div>
+    );
+  }
+
    // in useEffect, make a fetch get call to items/:id
    useEffect(() => {
        async function getItem() {
@@ -31,26 +48,28 @@ function SingleProduct() {
    // to see an example of how to use DisplayMany, look at SearchBar, where it is used to handle tags
    /// consult the Search Views / Products Page on our mockframe for how it should look
    // For reference on how the html should perhaps be structured, consider looking in f12 at an Etsy product page like this one: https://www.etsy.com/listing/1178358934/call-me-if-you-get-lost-vintage?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=Cute+Clothes&ref=sr_gallery-1-29&pro=1&pop=1&content_source=70e37ed25d53b15d87355691146c9ba55928239f%253A1178358934&search_preloaded_img=1&organic_search_click=1
+   const photos = product.additional_photos.map((url)=>{return {"photo":url}})
+   console.log(photos)
    return (
-   <div>
-    <h1>{product.name}</h1>
-    <p>{product.description}</p>
-    <p>Price: ${product.price}</p>
-
-{/* muliple photos are not displayed */}
-    <h3>Photos:</h3>
-      <DisplayMany items={product.additional_photos} />
-
-      {/* Display tags * tags are now displaying/}
-      <h3>Tags:</h3>
-      <DisplayMany items={product.tags} />
-
-      {/* Sample image display */}
+   <div className="flex-v">
+    <div className="flex-h">
+                {/* muliple photos are now displayed */}
+      <DisplayMany data={photos} factory={createPhoto} additionalClasses={"flex-v flex-start fit-to-parent"}/>
       {product.default_photo && (
         <div>
           <img src={product.default_photo} alt={product.name} />
         </div>
       )}
+      <div>
+      <h1>{product.name}</h1>
+    <p>{product.description}</p>
+    <p>Price: ${product.price}</p>
+    <button className="three-d-button" onClick={()=>{addToCart(product.id)}}>Add To Cart</button>
+      {/* muliple tags are now displayed */}
+      <DisplayMany data={product.tags.map((text)=>{return {"text":text}})} factory={createTag}additionalClasses={"flex-h"} />
+      </div>
+      </div>
+      <div className="desc-box">{product.description}</div>
    </div>
 );
 
