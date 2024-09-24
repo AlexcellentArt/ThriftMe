1afe299cf8a93a1fef24f12d0298fbc25ac397cf
Sujoy Manir <sujoymanir@gmail.com>
2024-09-24 13:29:55 -0400

    Comments for Account Not Done Yet..

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -2,7 +2,36 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "./AuthContext";
 import { useContext,useState } from "react";
 import DisplayMany from "./DisplayMany";
+import { useEffect } from "react";
+import React from "react";
+
+/**
+ * I need to have multiple buttons that will lead to the:
+ * Summary, 
+ * Personal Info, 
+ * Shop, 
+ * History, 
+ * Credit Cards,
+ *  Addresses, 
+ * Settings 
+ * API calls should be user ID specific so only the client can see the actual information
+ * So the API call should be for the database specific to them
+ * 
+ * Use effect can be used to pull for these.
+ * If the user is able to see their account they would have been able to only their own information on the page 
+ */
+
+
 function Account() {
+
+    useEffect(()=>{
+        const 
+
+
+
+    }
+
+
     return (<div>ACCOUNT</div>);
 }
 
