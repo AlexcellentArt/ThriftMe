f7276c327a286b1ab2f6f907f99cc0379b73c616
Alexis <alexcellentart@gmail.com>
2024-09-24 01:47:12 -0400

    Pagewrapper had main changed to body and then it wrapped in it. React is complaining, might need to fix later.

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -2,16 +2,16 @@ import thriftmeLogo from "/src/assets/ThriftMeLogo.svg";
 import Navigations from "./Navigations";
 function PageWrapper({children}) {
   return (
-    <>
+    <body>
       <header>
         <div>
           <img src={thriftmeLogo} className="logo hover" alt="ThriftMe logo" />
         </div>
         <Navigations/>
       </header>
-      <body>{children}</body>
+      <main>{children}</main>
       <footer>This is A Footer in PageWrapper. If you see this</footer>
-    </>
+    </body>
   );
 }
 
