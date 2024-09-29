09dfd4c11620cb60e751859002720d475156d9bd
Alexis <alexcellentart@gmail.com>
2024-09-29 00:01:04 -0400

    seller_id added to search context

--- src/components/SearchContext.jsx
+++ src/components/SearchContext.jsx
@@ -1,7 +1,8 @@
 import React, { useState, createContext } from "react";
 const SearchContext = createContext("SearchContext");
 export function SearchContextProvider({ children }) {
-  const [searchParams, setSearchParams] = useState({"search_text":undefined,"tags":[]});
+  const [searchParams, setSearchParams] = useState({"search_text":undefined,"tags":[],"seller_id":undefined});
+  
   const API_URL = "http://localhost:3000/api/";
   return (
     <SearchContext.Provider
