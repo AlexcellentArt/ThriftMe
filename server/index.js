05af8f9a7f4bfa0b5a4cb619052aae68a405c3ca
Alexis <alexcellentart@gmail.com>
2024-10-03 00:31:52 -0400

    commented out api

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.all('*',(req, res, next)=>{
     next();
   });
 // Api Routes
-app.use("/api",require("../api/index.js"));
+// app.use("/api",require("../api/index.js"));
 
 // Error Handling Middleware
 app.use((error,req,res,next)=>{
