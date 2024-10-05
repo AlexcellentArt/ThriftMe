1acd9ddcb403a5dfdc6f8e09ec7963d5b7dd0e90
Alexis <alexcellentart@gmail.com>
2024-10-05 01:21:10 -0400

    printing added to content prior to reset added.

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -12,10 +12,11 @@ function PageWrapper({children}) {
   let location = useLocation()
   // effect cleans up additonal context from previous page
   useEffect(() => {
+    console.log("conent prior to reset: ", additonalContent)
     setAdditonalContent(null);
   }, [location]);
   useEffect(() => {
-    console.log("additonal content changed!: ",additonalContent)
+    console.log("additonal content changed!: ", additonalContent)
   }, [additonalContent]);
 useEffect(() => {
   checkForLocalToken()
